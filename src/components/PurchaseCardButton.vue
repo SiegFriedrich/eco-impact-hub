2a9f38a07340592b34320b0a41c7e6c246189ddd
Sieg Friedrich <siegfriedrich@outlook.com>
2023-08-26 18:03:10 +0900

    modify detail page

---
+++ src/components/PurchaseCardButton.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="purchase-button">
+        <el-button type="info" size="large" round>
+            <div class="inside-button">
+                <span>{{ name }}</span>
+                <span>+</span>
+            </div>
+        </el-button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue';
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+})
+
+const { name } = toRefs(props);
+
+</script>
+
+<style scoped lang="scss">
+.purchase-button {
+    margin-bottom: 9px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .inside-button {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 50px;
+        width: 30vh;
+    }
+}
+
+.purchase-button {
+    el-button:hover {
+        box-shadow: 0px 4px 4px #00000010;
+    }
+}
+</style>
