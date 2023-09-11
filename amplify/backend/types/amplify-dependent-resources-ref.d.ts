e08ae4b416e4420879adcbf71c38dfd73b6f3bf9
Sieg Friedrich <siegfriedrich@outlook.com>
2023-09-11 21:45:47 +0900

    add file

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,20 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "function": {
+    "S3Triggeraa2f9b2b": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "hubMetaStorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
